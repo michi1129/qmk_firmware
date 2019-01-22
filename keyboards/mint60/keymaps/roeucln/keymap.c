4fe0e012e37d01d6e6d535a4f897cf3e5a451457
ro <ro.michi1129@gmail.com>
2019-01-23 00:07:39 +0900

    fix left mod keys

--- keyboards/mint60/keymaps/roeucln/keymap.c
+++ keyboards/mint60/keymaps/roeucln/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_COMM, KC_DOT,  KC_SCLN,   KC_M,    KC_R,    KC_D,    KC_Y,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_LCTL,     KC_A,    KC_O,    KC_E,    KC_I,    KC_U,      KC_G,    KC_T,    KC_K,    KC_S,    KC_N,    KC_QUOT,        KC_ENT,   \
     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_F,      KC_B,    KC_H,    KC_J,    KC_L,    KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
-    KC_ESC,    KC_CAPS,    KC_LGUI,    KC_LALT,    KC_SPC,        KC_BSPC, KC_ENT,  KC_RGUI,                    KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_ESC,    KC_CAPS,    KC_LALT,    KC_LGUI,    KC_SPC,        KC_BSPC, KC_ENT,  KC_RGUI,                    KC_LEFT,KC_DOWN,KC_RGHT \
   ),
   [1] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
